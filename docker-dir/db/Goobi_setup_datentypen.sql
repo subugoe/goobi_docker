f01c742e1dfa5d60e426e5f91afbed53d8cc8486
Joerg Panzer <panzer@sub.uni-goettingen.de>
2015-06-15 15:16:52 +0200

    set correct table name datentypen, instead of benutzer

--- docker-dir/db/Goobi_setup_datentypen.sql
+++ docker-dir/db/Goobi_setup_datentypen.sql
@@ -1,6 +1,6 @@
-/*!40000 ALTER TABLE `benutzer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `datentypen` DISABLE KEYS */;
 INSERT INTO `datentypen` (`DatentypenID`,`Bezeichnung`) VALUES (1,'zahl');
 INSERT INTO `datentypen` (`DatentypenID`,`Bezeichnung`) VALUES (2,'string');
 INSERT INTO `datentypen` (`DatentypenID`,`Bezeichnung`) VALUES (3,'boolen');
 INSERT INTO `datentypen` (`DatentypenID`,`Bezeichnung`) VALUES (4,'datum');
-/*!40000 ALTER TABLE `benutzer` ENABLE KEYS */;
+/*!40000 ALTER TABLE `datentypen` ENABLE KEYS */;
