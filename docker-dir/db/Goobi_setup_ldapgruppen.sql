88f80977beb7556bdcebb5c98b29d277b9c4bb1d
Joerg Panzer <panzer@sub.uni-goettingen.de>
2015-06-15 15:16:12 +0200

    set correct table name ldapgruppen, instead of benutzer

--- docker-dir/db/Goobi_setup_ldapgruppen.sql
+++ docker-dir/db/Goobi_setup_ldapgruppen.sql
@@ -1,5 +1,5 @@
-/*!40000 ALTER TABLE `benutzer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ldapgruppen` DISABLE KEYS */;
 INSERT INTO `ldapgruppen` (
   `ldapgruppenID`, `titel`) VALUES
   (2, 'test'); /* id 2 has to be inserted to match users (benutzer)*/
-/*!40000 ALTER TABLE `benutzer` ENABLE KEYS */;
+/*!40000 ALTER TABLE `ldapgruppen` ENABLE KEYS */;
