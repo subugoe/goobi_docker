e68e97ebc670fe492c78e8115ca4c2c6d7b973f9
Dennis Neumann <neumann@sub.uni-goettingen.de>
2015-06-15 17:06:48 +0200

    adding more content to the database

--- docker-dir/db-content/fill_db.sh
+++ docker-dir/db-content/fill_db.sh
@@ -4,5 +4,11 @@
 
 mysqladmin --host=goobi -u $MYSQL_USER --password=$MYSQL_PASSWORD --silent --wait=30 ping || exit 1
 cat 	/tmp/Goobi_setup_schema.sql \
-	/tmp/Goobi_setup_default.sql \
+	/tmp/Goobi_setup_ldapgruppen.sql \
+	/tmp/Goobi_setup_benutzer.sql \
+	/tmp/Goobi_setup_benutzergruppen.sql \
+	/tmp/Goobi_setup_benutzergruppenmitgliedschaft.sql \
+	/tmp/Goobi_setup_regelsaetze.sql \
+	/tmp/Goobi_setup_laufzettel.sql \
+	/tmp/Goobi_setup_datentypen.sql \
 	| mysql --host=goobi -u $MYSQL_USER --password=$MYSQL_PASSWORD -D $MYSQL_DATABASE
