18d7fcc7ca0959278fcdc304d2c00c2680713224
Joerg Panzer <panzer@sub.uni-goettingen.de>
2015-06-17 11:55:57 +0200

    remove sleep; change order (database cration and insert before tomcat/goobi start)

--- docker-dir/start_all.sh
+++ docker-dir/start_all.sh
@@ -6,7 +6,9 @@ docker build -t dockerdir_goobitomcat ./tomcat
 
 
 docker run --name db1 --env-file docker-env.yml --publish 3306:3306 -d dockerdir_goobi
-docker run --name tomcat1 --link db1:goobi --volume=${PWD}/tomcat/goobi2.0:/usr/local/tomcat/webapps/goobi --publish 8080:8080 -d dockerdir_goobitomcat
-sleep 15
 docker run --name content1 --env-file docker-env.yml --link db1:goobi dockerdir_dbcontent
+
+docker run --name tomcat1 --link db1:goobi --volume=${PWD}/tomcat/goobi2.0:/usr/local/tomcat/webapps/goobi --publish 8080:8080 -d dockerdir_goobitomcat
+#sleep 60
+
 
