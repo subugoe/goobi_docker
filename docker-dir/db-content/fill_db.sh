b627ca0b4de18ad505a69980a2f1b6a142d5c451
Joerg Panzer <panzer@sub.uni-goettingen.de>
2015-06-19 15:49:18 +0200

    add full dump (dump is not in the git repo

--- docker-dir/db-content/fill_db.sh
+++ docker-dir/db-content/fill_db.sh
@@ -2,15 +2,20 @@
 
 
 
+#mysqladmin --host=goobi -u $MYSQL_USER --password=$MYSQL_PASSWORD --silent --wait=30 ping || exit 1
+#cat 	/tmp/Goobi_setup_schema.sql \
+#	/tmp/Goobi_setup_ldapgruppen.sql \
+#	/tmp/Goobi_setup_benutzer.sql \
+#	/tmp/Goobi_setup_benutzergruppen.sql \
+#	/tmp/Goobi_setup_benutzergruppenmitgliedschaft.sql \
+#	/tmp/Goobi_setup_regelsaetze.sql \
+#	/tmp/Goobi_setup_laufzettel.sql \
+#	/tmp/Goobi_setup_datentypen.sql \
+#	/tmp/Goobi_setup_version.sql \
+#	/tmp/VD18.sql \
+#	| mysql --default-character-set="UTF8" --host=goobi -u $MYSQL_USER --password=$MYSQL_PASSWORD -D $MYSQL_DATABASE
+
 mysqladmin --host=goobi -u $MYSQL_USER --password=$MYSQL_PASSWORD --silent --wait=30 ping || exit 1
 cat 	/tmp/Goobi_setup_schema.sql \
-	/tmp/Goobi_setup_ldapgruppen.sql \
-	/tmp/Goobi_setup_benutzer.sql \
-	/tmp/Goobi_setup_benutzergruppen.sql \
-	/tmp/Goobi_setup_benutzergruppenmitgliedschaft.sql \
-	/tmp/Goobi_setup_regelsaetze.sql \
-	/tmp/Goobi_setup_laufzettel.sql \
-	/tmp/Goobi_setup_datentypen.sql \
-	/tmp/Goobi_setup_version.sql \
-	/tmp/VD18.sql \
+	/tmp/alles.sql \
 	| mysql --default-character-set="UTF8" --host=goobi -u $MYSQL_USER --password=$MYSQL_PASSWORD -D $MYSQL_DATABASE
